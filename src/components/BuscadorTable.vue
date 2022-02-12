4dc2e6b679504e07de1b0fadff3a0264b560bae9
rhchaves <rodolfohenriquechaves@gmail.com>
2022-02-12 17:41:54 -0300

    Criado componente de tabela dinâmica

---
+++ src/components/BuscadorTable.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-page>
+
+    <div class="q-pa-md">
+      <q-table
+        :title="title"
+        :data="data"
+        :columns="columns"
+        :row-key="rowKey"
+        :selected-rows-label="getSelectedString"
+        :selection="selection"
+        :selected.sync="selected"
+      />
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'BuscadorTable',
+
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    columns: {
+      type: Array,
+      required: true,
+    },
+    data: {
+      type: Array,
+      required: true,
+    },
+    rowKey: {
+      type: String,
+      required: false,
+      default: 'name',
+    },
+    selection: {
+      type: String,
+      required: false,
+      default: 'single',
+    },
+  },
+
+  data() {
+    return {
+      selected: [],
+
+    };
+  },
+  methods: {
+    getSelectedString() {
+      return this.selected.length === 0 ? '' : `${this.selected.length} record${this.selected.length > 1 ? 's' : ''} selected of ${this.data.length}`;
+    },
+  },
+};
+</script>
